d38217e988484a5a45169e0db01b788bc3865baa
Joeeazy <joeeazy34@gmail.com>
2024-12-28 12:48:48 +0300

    error fix

--- client/src/scenes/layout/Layout.jsx
+++ client/src/scenes/layout/Layout.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
-//import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
-// import { useGetUserQuery } from "state/api";
+import { useGetUserQuery } from "state/api";
+import { useSelector } from "react-redux";
 
 export default function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)")
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const userId = useSelector((state) => state.global.userId);
+
+  const { data } = useGetUserQuery(userId)
+  console.log("data", data)
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar isNonMobile={isNonMobile}
